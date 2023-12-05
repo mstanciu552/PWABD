a025756e7d9a3ee143b461549b399c6d5714f1ec
Mihai Stanciu <mstanciu552@gmail.com>
2023-12-05 08:48:10 +0200

    tema2

--- Tema2/conversie_distanta/src/main/java/pwabd/ConversieServlet.java
+++ Tema2/conversie_distanta/src/main/java/pwabd/ConversieServlet.java
@@ -1,9 +1,65 @@
 package pwabd;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Optional;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import pwabd.conversie.InfoConversie;
+import pwabd.util.Eroare;
+import pwabd.util.Unitati;
 
 @WebServlet("/conversie")
 public class ConversieServlet extends HttpServlet {
-    
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader buffer = req.getReader();
+        String line;
+
+        while ((line = buffer.readLine()) != null)
+            sb.append(line);
+
+        String requestBody = sb.toString();
+        System.out.println(requestBody);
+
+        String[] body = requestBody.split("&");
+        double valoare = Double.parseDouble(body[0].split("=")[1]);
+        String din = body[1].split("=")[1];
+        String catre = body[2].split("=")[1];
+
+        Unitati unitateDin = null;
+        Unitati unitateCatre = null;
+
+        if (din == "metri")
+            unitateDin = Unitati.METRI;
+        else if (din == "inch")
+            unitateDin = Unitati.INCH;
+        else
+            unitateDin = Unitati.FEET;
+
+        if (catre == "metri")
+            unitateCatre = Unitati.METRI;
+        else if (catre == "inch")
+            unitateCatre = Unitati.INCH;
+        else
+            unitateCatre = Unitati.FEET;
+
+        InfoConversie conversie = new InfoConversie();
+        conversie.setValoareDeconvertit(valoare);
+        conversie.setUnitateDin(unitateDin);
+        conversie.setUnitateCatre(unitateCatre);
+
+        Optional<Eroare> rezultat_conversie = conversie.converteste();
+
+        // Daca Optional e empty => fara eroare
+        if (!rezultat_conversie.isPresent())
+            resp.sendRedirect("/conversie_distanta/infoConv.jsp");
+    }
 }
--- Tema2/conversie_distanta/src/main/java/pwabd/conversie/InfoConversie.java
+++ Tema2/conversie_distanta/src/main/java/pwabd/conversie/InfoConversie.java
@@ -1,46 +1,90 @@
 package pwabd.conversie;
 
 import java.io.Serializable;
+import java.util.Optional;
 
+import pwabd.util.Eroare;
 import pwabd.util.Unitati;
 
 public class InfoConversie implements Serializable {
-    private int valoareDeconvertit;
-    private int valoareConvertita;
+    private double valoareDeconvertit;
+    private double valoareConvertita;
     private double constantaDeConversie;
     private Unitati unitateDin;
     private Unitati unitateCatre;
 
-    public int getValoareDeconvertit() {
+    public Optional<Eroare> converteste() {
+        Eroare eroare = null;
+        switch (unitateDin) {
+            case METRI:
+                if (unitateCatre == Unitati.INCH) {
+                    valoareConvertita = 39.37 * valoareDeconvertit;
+                } else if (unitateCatre == Unitati.FEET) {
+                    valoareConvertita = 3.28 * valoareDeconvertit;
+                } else
+                    return Optional.of(eroare);
+                break;
+
+            case INCH:
+                if (unitateCatre == Unitati.METRI) {
+                    valoareConvertita = valoareDeconvertit / 39.37;
+                } else if (unitateCatre == Unitati.FEET) {
+                    valoareConvertita = 0.083 * valoareDeconvertit;
+                } else
+                    return Optional.of(eroare);
+                break;
+
+            case FEET:
+                if (unitateCatre == Unitati.INCH) {
+                    valoareConvertita = valoareDeconvertit * 12;
+                } else if (unitateCatre == Unitati.METRI) {
+                    valoareConvertita = 0.3048 * valoareDeconvertit;
+                } else
+                    return Optional.of(eroare);
+                break;
+
+        }
+        return Optional.empty();
+    }
+
+    public double getValoareDeconvertit() {
         return valoareDeconvertit;
     }
-    public void setValoareDeconvertit(int valoareDeconvertit) {
+
+    public void setValoareDeconvertit(double valoareDeconvertit) {
         this.valoareDeconvertit = valoareDeconvertit;
     }
-    public int getValoareConvertita() {
+
+    public double getValoareConvertita() {
         return valoareConvertita;
     }
-    public void setValoareConvertita(int valoareConvertita) {
+
+    public void setValoareConvertita(double valoareConvertita) {
         this.valoareConvertita = valoareConvertita;
     }
+
     public double getConstantaDeConversie() {
         return constantaDeConversie;
     }
+
     public void setConstantaDeConversie(double constantaDeConversie) {
         this.constantaDeConversie = constantaDeConversie;
     }
+
     public Unitati getUnitateDin() {
         return unitateDin;
     }
+
     public void setUnitateDin(Unitati unitateDin) {
         this.unitateDin = unitateDin;
     }
+
     public Unitati getUnitateCatre() {
         return unitateCatre;
     }
+
     public void setUnitateCatre(Unitati unitateCatre) {
         this.unitateCatre = unitateCatre;
     }
-    
 
 }
